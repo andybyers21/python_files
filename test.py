1db18d7a1d69277f7c530a18132c845f44a7e87f
andybyers21 <andy.byers21@gmail.com>
2020-09-11 19:25:14 +0100

    updated test with classes & objects

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
 
-class MyClass:
-    variable = "blah"
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
 
-    def function(self):
-        print("This is a message inside the class.")
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (
+            self.name, self.color, self.kind, self.value
+        )
+        return desc_str
 
-myobjectx = MyClass()
+
+car1 = Vehicle()
+
+car1.name = "Fer"
+car1.kind = "convertible"
+car1.color = "red"
+car1.value = 60000.00
+
+# car2 = Vehi
+
+print(car1.description())
+# print(car2.description())
