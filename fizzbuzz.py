7a60be9af8810df3ed01959980a786d8f3d3c0d4
andybyers21 <andy.byers21@gmail.com>
2020-09-13 17:43:51 +0100

    did a fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,13 @@
-for a in range(1, 101):
-    if a % 3 == 0 and a % 5 == 0:
-        print("FIZZBUZZ")
-    elif a % 3 == 0:
-        print("FIZZ")
-    elif a % 5 == 0:
-        print("BUZZ")
-    else:
-        print(a)
-    a += 1
+def fizzle(x, y):
+    for a in range(x, y):
+        if a % 3 == 0 and a % 5 == 0:
+            print("FIZZBUZZ")
+        elif a % 3 == 0:
+            print("FIZZ")
+        elif a % 5 == 0:
+            print("BUZZ")
+        else:
+            print(a)
+
+
+fizzle(1, 50)
