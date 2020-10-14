1d69da38fe3b4574eecb5765c3f5054b73b04528
andybyers21 <andy.byers21@gmail.com>
2020-10-14 13:13:25 +0100

    add random password generator

---
+++ password.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+
+def password(len, chars=string.ascii_letters + string.digits + string.punctuation):
+    """ Generate a random password string with user defined amount of characters. """
+    return "".join(random.choice(chars) for _ in range(len))
+
+
+print(password(int(input('How many characters in your password?'))))
